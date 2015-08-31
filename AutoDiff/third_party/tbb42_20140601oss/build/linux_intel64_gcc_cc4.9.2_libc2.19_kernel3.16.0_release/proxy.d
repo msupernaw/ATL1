5f488adbbbe9a76cbb3f24a6aeef3e572920dc36
msupernaw <msupernaw@gmail.com>
2015-08-31 16:22:52 -0400

    deleted unneeded stuff

--- AutoDiff/third_party/tbb42_20140601oss/build/linux_intel64_gcc_cc4.9.2_libc2.19_kernel3.16.0_release/proxy.d
+++
@@ -1,2 +0,0 @@
-proxy.o: ../../src/tbbmalloc/proxy.cpp ../../src/tbbmalloc/proxy.h \
- ../../include/tbb/tbb_config.h
--- AutoDiff/third_party/tbb42_20140601oss/build/linux_intel64_gcc_cc4.9.2_libc2.19_kernel3.16.0_release/x86_rtm_rw_mutex.d
+++
@@ -1,2 +0,0 @@
-x86_rtm_rw_mutex.o: ../../src/tbb/x86_rtm_rw_mutex.cpp \
- ../../include/tbb/tbb_config.h
